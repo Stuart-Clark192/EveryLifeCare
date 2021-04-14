585851946fc0144cec2dd678e08bea548be7c11c
Stuart Clark <>
2021-04-14 09:28:16 +0100

    Initial Project setup

---
+++ EveryLifeCare/EveryLifeCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  EveryLifeCareApp.swift
+//  EveryLifeCare
+//
+//  Created by Stuart on 14/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct EveryLifeCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
